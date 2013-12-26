822fb9d91b682a16069720a9bf07160eae39ef2a
unknown <Enrique@Enrique-PC.library.bc.ad>
2013-12-26 16:20:34 -0500

    Added smaple directives and module services

--- public/client.js
+++ public/client.js
@@ -1,75 +1,115 @@
 (function(){
-	var starterApp = angular.module("starterApp", ['ngRoute']);
+    var starterApp = angular.module("starterApp", ["ngRoute", "customDirective"]);
 
-	starterApp.config(function($routeProvider){
-		// Setting The Routes
-		$routeProvider
-			.when("/",
-				{
-					controller:"homeController",
-					templateUrl:"views/home.html"
-				})
-			.when("/about",
-				{
-					controller:"aboutController",
-					templateUrl:"views/about.html"
-				})
-			.when("/contact",
-				{
-					controller:"contactController",
-					templateUrl:"views/contactus.html"
-				})
-			.otherwise({ redirectTo:"/"});
-	});
+    starterApp.config(function($routeProvider){
+        // Setting The Routes
+        $routeProvider
+            .when("/",
+                {
+                    controller:"homeController",
+                    templateUrl:"views/home.html"
+                })
+            .when("/about",
+                {
+                    controller:"aboutController",
+                    templateUrl:"views/about.html"
+                })
+            .when("/contact",
+                {
+                    controller:"contactController",
+                    templateUrl:"views/contactus.html"
+                })
+            .otherwise({ redirectTo:"/"});
+    });
 
-	//////// Module Styles /////////
-	starterApp.factory('starterAppFactory', function() {
-		var object = []; // Ajax data
-		var factory = {};
-		factory.sayHello = function() {
-	            return "Hello";
-	   	};
+    //////// Testing Directives  /////////
+    starterApp.directive('directiveElement', function(){ // Element example
+        return {
+            restrict:"E",
+            link: function(scope, elem, attr){
+                scope.title = attr.title;
+            },
+            replace:true,
+            template: "<h1>{{title}}</h1>"
+        }
+    });
 
-	   	factory.addObject = function(p){
-	   		people.push(p);
-	    };
+    //////// Testing Modules Services /////////
+    var MyFunc = function() {
+        this.name = "default name";
 
-	   	factory.getObject = function(){
-	   		return object;
-	    };
-	    return factory;
-	});
-	    
-	// Controllers
-	var controllers = {};
+        this.$get = function() {
+            this.name = "new name"
+            return "Hello from MyFunc.$get(). this.name = " + this.name;
+        };
 
-	// Controlelr with hardcoded Data
-	controllers.homeController = function($scope){
-		$scope.pageName = "Home";
-		$scope.people = [
-			{ name: "Edward", sex: "male" },
-			{ name: "Enri", sex: "male" },
-			{ name: "Lisa", sex: "female" },
-			{ name: "Mike", sex: "male" }
-		];
+        return "Hello from MyFunc(). this.name = " + this.name;
+    };
+    // returns the actual function
+    starterApp.service( 'myService', MyFunc );
 
-		$scope.add = function(){
-			$scope.people.push({name:$scope.newPerson.name, sex:"male"});
-		}
+    // returns the function's return value
+    starterApp.factory( 'myFactory', MyFunc );
 
-	};
+    // returns the output of the function's $get function
+    starterApp.provider( 'myProv', MyFunc );
 
-	// Controller using "Factory"
-	controllers.aboutController = function($scope, starterAppFactory){
-		$scope.pageName = "About";
-		$scope.people = starterAppFactory.getObject();
-	};
+    starterApp.factory('starterAppFactory', function() {
+        var object = []; // Ajax data
+        var factory = {};
+        factory.sayHello = function() {
+                return "Hello";
+        };
 
-	// Controller Deafult
-	controllers.contactController = function($scope){
-		$scope.pageName = "Contact";
-		$scope.content = "Information about contact us goes here";
-	};
-	// ...
-	starterApp.controller(controllers);
+        factory.addObject = function(p){    
+            people.push(p);
+        };
+
+        factory.getObject = function(){
+            return object;
+        };
+        return factory;
+    });
+        
+    // Controllers
+    var controllers = {};
+
+    // Controller with hardcoded Data
+    controllers.homeController = function($scope){
+        $scope.pageName = "Home";
+        $scope.people = [
+            { name: "Edward", sex: "male" },
+            { name: "Enri", sex: "male" },
+            { name: "Lisa", sex: "female" },
+            { name: "Mike", sex: "male" }
+        ];
+
+        $scope.add = function(){
+            if($scope.newPerson && $scope.newPerson.name.length != 0){
+                $scope.people.push({name:$scope.newPerson.name, sex:"male"});
+            }
+        }
+
+    };
+
+    // Controller using "Factory"
+    controllers.aboutController = function($scope, starterAppFactory){
+        $scope.pageName = "About";
+        $scope.content = "Information about info goes here";
+        // $scope.people = starterAppFactory.getObject();
+    };
+
+    // Controller Deafult
+    controllers.contactController = function($scope){
+        $scope.pageName = "Contact";
+    };
+
+    controllers.testingServicesController = function($scope, myService, myFactory, myProv ){
+        $scope.serviceOutput = "myService = " + myService;
+        $scope.factoryOutput = "myFactory = " + myFactory;
+        $scope.providerOutput = "myProvider = " + myProv;
+    };
+
+    // ...
+    starterApp.controller(controllers);
 }());
---
+++ public/directives/customDirective.js
@@ -0,0 +1,13 @@
+angular.module("customDirective", [])
+    .directive("namer", function(){ // Class example
+        return {
+            scope:{},
+            restrict:"C",
+            link: function(scope, elem, attr){
+                scope.full = attr.name + " " + attr.num;
+                scope.name = attr.name;
+                scope.num = attr.num;
+            },
+            template: "{{name}} {{num}}"
+        }
+});
